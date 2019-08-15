b94aeae1a120b00d4015ffbeda05cd50fcb46324
anhquoc09 <anhquoc.haq09@gmail.com>
2019-08-15 16:20:22 +0700

    clone PagerSnapHelper

--- app/src/main/java/com/example/anhquoc/mycustom/ListPagerSnapHelper.kt
+++ app/src/main/java/com/example/anhquoc/mycustom/ListPagerSnapHelper.kt
@@ -1,8 +1,6 @@
 package com.example.anhquoc.mycustom
 
-import android.support.v7.widget.OrientationHelper
-import android.support.v7.widget.PagerSnapHelper
-import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.*
 import android.view.View
 import kotlin.math.abs
 
@@ -12,21 +10,20 @@ import kotlin.math.abs
  * On 12/08/2019
  */
 class ListPagerSnapHelper : PagerSnapHelper() {
-
     private var verticalHelper: OrientationHelper? = null
-
     private var horizontalHelper: OrientationHelper? = null
 
-    override fun calculateDistanceToFinalSnap(layoutManager: RecyclerView.LayoutManager, targetView: View): IntArray? {
+    override fun calculateDistanceToFinalSnap(layoutManager: RecyclerView.LayoutManager,
+                                              targetView: View): IntArray? {
         val out = IntArray(2)
         if (layoutManager.canScrollHorizontally()) {
-//            out[0] = distanceToCenter(targetView, getHorizontalHelper(layoutManager))
+            out[0] = distanceToCenter(layoutManager, targetView, getHorizontalHelper(layoutManager))
         } else {
             out[0] = 0
         }
 
         if (layoutManager.canScrollVertically()) {
-//            out[1] = distanceToCenter(targetView, getVerticalHelper(layoutManager))
+            out[1] = distanceToCenter(layoutManager, targetView, getVerticalHelper(layoutManager))
         } else {
             out[1] = 0
         }
@@ -48,9 +45,9 @@ class ListPagerSnapHelper : PagerSnapHelper() {
         }
         var startMostChildView: View? = null
         if (layoutManager.canScrollVertically()) {
-            startMostChildView = findCenterView(layoutManager, getVerticalHelper(layoutManager))
+            startMostChildView = findStartView(layoutManager, getVerticalHelper(layoutManager))
         } else if (layoutManager.canScrollHorizontally()) {
-            startMostChildView = findCenterView(layoutManager, getHorizontalHelper(layoutManager))
+            startMostChildView = findStartView(layoutManager, getHorizontalHelper(layoutManager))
         }
 
         if (startMostChildView == null) {
@@ -80,7 +77,19 @@ class ListPagerSnapHelper : PagerSnapHelper() {
             if (forwardDirection) centerPosition + 1 else centerPosition
     }
 
-    private fun findCenterView(layoutManager: RecyclerView.LayoutManager, helper: OrientationHelper): View? {
+    private fun distanceToCenter(layoutManager: RecyclerView.LayoutManager,
+                                 targetView: View, helper: OrientationHelper): Int {
+        val childCenter = helper.getDecoratedStart(targetView) + helper.getDecoratedMeasurement(targetView) / 2
+        val containerCenter: Int = if (layoutManager.clipToPadding) {
+            helper.startAfterPadding + helper.totalSpace / 2
+        } else {
+            helper.end / 2
+        }
+        return childCenter - containerCenter
+    }
+
+    private fun findCenterView(layoutManager: RecyclerView.LayoutManager,
+                               helper: OrientationHelper): View? {
         val childCount = layoutManager.childCount
         if (childCount == 0) {
             return null
@@ -90,7 +99,7 @@ class ListPagerSnapHelper : PagerSnapHelper() {
         val center: Int = if (layoutManager.clipToPadding) {
             helper.startAfterPadding + helper.totalSpace / 2
         } else {
-            helper.totalSpace / 2
+            helper.end / 2
         }
         var absClosest = Integer.MAX_VALUE
 
@@ -107,19 +116,38 @@ class ListPagerSnapHelper : PagerSnapHelper() {
         return closestChild
     }
 
-    private fun distanceToCenter(helper: OrientationHelper): Int {
-        return helper.end - helper.startAfterPadding
+    private fun findStartView(layoutManager: RecyclerView.LayoutManager,
+                              helper: OrientationHelper): View? {
+        val childCount = layoutManager.childCount
+        if (childCount == 0) {
+            return null
+        }
+
+        var closestChild: View? = null
+        var startest = Integer.MAX_VALUE
+
+        for (i in 0 until childCount) {
+            val child = layoutManager.getChildAt(i)
+            val childStart = helper.getDecoratedStart(child)
+
+            /** if child is more to start than previous closest, set it as closest   */
+            if (childStart < startest) {
+                startest = childStart
+                closestChild = child
+            }
+        }
+        return closestChild
     }
 
     private fun getVerticalHelper(layoutManager: RecyclerView.LayoutManager): OrientationHelper {
-        if (verticalHelper?.layoutManager !== layoutManager) {
+        if (verticalHelper == null || verticalHelper!!.layoutManager !== layoutManager) {
             verticalHelper = OrientationHelper.createVerticalHelper(layoutManager)
         }
         return verticalHelper!!
     }
 
     private fun getHorizontalHelper(layoutManager: RecyclerView.LayoutManager): OrientationHelper {
-        if (horizontalHelper?.layoutManager !== layoutManager) {
+        if (horizontalHelper == null || horizontalHelper!!.layoutManager !== layoutManager) {
             horizontalHelper = OrientationHelper.createHorizontalHelper(layoutManager)
         }
         return horizontalHelper!!
